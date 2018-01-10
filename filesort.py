6ff661e8af470aba2bf8a52c03900cf714cba190
Unknown <ruvan@berkeley.edu>
2018-01-10 15:26:31 -0800

    fixed bugs

--- filesort.py
+++ filesort.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-import os
-import shutil
+import os, shutil, logging
 
+#TODO add loggging
+#logging.basicConfig(filename='filesort.log')
 
 audio_tags = [".mp3",".flac"]
 video_tags = [".mkv",".mp4"] # unused
 
-audio_dir = '~/Music/'
-video_dir = '~/Videos/'
-root = '~/misc/'
-
+audio_dir = os.path.expanduser('~/Music/')
+video_dir = os.path.expanduser('~/Videos/')
+root = os.path.expanduser('~/misc')
+preconditions = [audio_dir,video_dir]
 def isMusic(fn):
     for tag in audio_tags:
         if(fn.endswith(tag)):
@@ -19,21 +20,33 @@ def isMusic(fn):
 def moveTo(fn_path, dest):
     try:
         shutil.move(fn_path,dest)
-    except shutil.Error:
-        print('Overwriting: ' + fn_path)
-        shutil.copy2(fn_path,dest)
-        shutil.remove(fn_path,dest)
+    except shutil.Error as e:
+        #Maybe do dif of folders for more advanced add-only copy
+        print(e)
 
-def containsMusic(fn):
-    for root, dirs, files in os.walk(fn):
+def containsMusic(fn_path):
+    for root, dirs, files in os.walk(fn_path):
         for name in files:
             if isMusic(name):
                 return True
     return False
 
 if __name__ == "__main__":
-    for fn in os.listdir(os.expanduser(root)):
+    for condition in preconditions:
+        if not os.path.exists(condition):
+            os.makedirs(condition)
+
+    for fn in os.listdir(root):
+        fn_path = os.path.join(root,fn)
         if fn.startswith('.'):
             continue
-        if os.isfile(fn):
+        if os.path.isfile(fn_path):
             if isMusic(fn):
+                moveTo(fn_path,audio_dir)
+            else:
+                moveTo(fn_path,video_dir)
+        else:
+            if containsMusic(fn_path):
+                moveTo(fn_path,audio_dir)
+            else:
+                moveTo(fn_path,video_dir)
